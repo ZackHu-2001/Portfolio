1f714263d31abb7101086037df03519b819a8146
zackhu's macbook air <zackhu2001@gmail.com>
2024-05-16 10:37:24 -0700

    Feat: add starry sky effect

---
+++ src/app/effects/StarrySky-pureSass/page.tsx
@@ -0,0 +1,13 @@
+import './main.css'
+
+export default function StarrySky() {
+    return (
+        <div className='bg w-full h-screen flex flex-col justify-center items-center'>
+            <h1>Starry Sky</h1>
+            <h2>Implemented with Sass</h2>
+            <div className='layer1'></div>
+            <div className='layer2'></div>
+            <div className='layer3'></div>
+        </div>
+    )
+}
